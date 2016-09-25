16589064358945734357cba946611086abd550b3
Asif R Choudhury <choudhury.asif@gmail.com>
2016-09-26 02:15:13 +0530

    TDD'd validation of SES event and that confirm talks to s3

--- functions/confirm/confirm.js
+++ functions/confirm/confirm.js
@@ -1,8 +1,35 @@
 var Confirm = function(s3, env) {
   var self = this;
 
+  env = env || process.env;
+
+  var sesRecords = function(e) {
+    return e.Records.filter(function(record) {
+      return record.eventSource === "aws:ses";
+    }).length > 0;
+  };
+
   self.handler = function(event, context, callback) {
-    callback();
+    if (!event.Records || !event.Records.length || !sesRecords(event)) {
+      var error = {
+        "error": "No SES Event Records"
+      };
+      callback(error);
+    }
+    else {
+      var params = {
+        "Bucket": env['AWS_S3_BUCKET'],
+        "Key": env['AWS_S3_KEY_PREFIX'] + event.Records[0].ses.mail.messageId
+      };
+      s3.getObject(params, function(err, data) {
+        if (err) {
+          callback(err);
+        }
+        else {
+          callback();
+        }
+      });
+    }
   };
   return self;
 };
--- functions/confirm/test/confirmSpec.js
+++ functions/confirm/test/confirmSpec.js
@@ -2,12 +2,151 @@ var should = require('should');
 
 var Confirm = require('../confirm').Confirm;
 
+var sampleEvent = require('./resources/sampleEvent.json');
+
+var validEvent = function() {
+  return JSON.parse(JSON.stringify(sampleEvent));
+}
+
+var mock = function(obj, methodName, cb) {
+  cb = cb || new Function();
+  obj[methodName] = function() {
+    obj[methodName].called = true;
+    obj[methodName].args = arguments;
+    cb.apply(obj, arguments);
+  };
+  obj[methodName].called = false;
+};
+
+var s3getObjectStub = function(params, cb) {
+  cb = cb || new Function();
+  var err = null;
+  var data = {
+    "Body": ""
+  };
+  cb(err, data);
+};
+
+var stubbedS3 = {
+  "getObject": s3getObjectStub
+};
 
 describe("Confirm", function() {
   describe("handler", function() {
-    it("should call third param as a callback", function(done) {
-      var confirm = new Confirm();
-      confirm.handler(null,null,done);
+    it("should call third parameter as a callback in order to conform to AWS Lambda", function(done) {
+      var confirm = new Confirm(stubbedS3);
+      var event = validEvent();
+
+      confirm.handler(event, null, function() {
+        done();
+      });
+    });
+
+    it("should require that there is a Record field", function(done) {
+      var confirm = new Confirm(stubbedS3);
+      var event = {}
+
+      confirm.handler(event, null, function(err, data) {
+        err.error.should.eql("No SES Event Records");
+        done();
+      });
+    });
+
+    it("should require there to be at least on Record", function(done) {
+      var confirm = new Confirm(stubbedS3);
+      var event = {
+        "Records": []
+      };
+
+      confirm.handler(event, null, function(err, data) {
+        err.error.should.eql("No SES Event Records");
+        done();
+      });
+    });
+
+    it("should require there to be at least one AWS SES record", function(done) {
+      var confirm = new Confirm(stubbedS3);
+      var event = {
+        "Records": [
+          {
+            "eventSource": "aws:s3",
+            "s3": {}
+          },
+          {
+            "eventSource": "aws:random",
+            "random": {}
+          }
+        ]
+      };
+
+      confirm.handler(event, null, function(err, data) {
+        err.error.should.eql("No SES Event Records");
+        done();
+      });
+    });
+
+    it("should not return any errors when a valid SES event is present", function(done) {
+      var confirm = new Confirm(stubbedS3);
+      var event = validEvent();
+
+      confirm.handler(event, null, function(err, data) {
+        should.not.exist(err);
+        done();
+      });
+    });
+
+    it("should retrieve bucket specified in environment", function(done) {
+      var s3 = {};
+      var env = {
+        "AWS_S3_BUCKET": "testbucket"
+      };
+      var confirm = new Confirm(s3,env);
+      var event = validEvent();
+
+      mock(s3,'getObject', s3getObjectStub);
+
+
+      confirm.handler(event, null, function(err, data) {
+        s3.getObject.called.should.eql(true);
+        s3.getObject.args[0].Bucket.should.eql(env['AWS_S3_BUCKET']);
+        done();
+      });
+    });
+
+    it("should retrieve s3 object using prefix and messageId", function(done) {
+      var s3 = {};
+      var env = {
+        "AWS_S3_KEY_PREFIX": "prefix/"
+      };
+      var confirm = new Confirm(s3,env);
+      var event = validEvent();
+      event.Records[0].ses.mail.messageId = "testMessageId";
+
+      mock(s3,'getObject', s3getObjectStub);
+
+
+      confirm.handler(event, null, function(err, data) {
+        s3.getObject.args[0].Key.should.eql("prefix/testMessageId");
+        done();
+      });
+    });
+
+    it("should return error upon s3 error", function(done) {
+      var s3 = {};
+      var env = {
+        "AWS_S3_BUCKET": "testbucket"
+      };
+      var confirm = new Confirm(s3,env);
+      var event = validEvent();
+
+      mock(s3,'getObject', function(params, cb) {
+        cb({"error": "S3 Error"});
+      });
+
+      confirm.handler(event, null, function(err, data) {
+        err.error.should.eql("S3 Error");
+        done();
+      });
     });
   });
 });
